ea2dd63d0dc5223d7f3fe5fbc1c16817c8a5b556
HeWriteMe <hewriteme@protonmail.com>
2025-11-25 16:29:46 -0500

    Vagrant tweaks

--- scripts/deploysection.sh
+++ scripts/deploysection.sh
@@ -4,4 +4,5 @@ CUR_PATH="$PWD"
 REL_TO_HOME="${CUR_PATH/#$HOME\//}"
 PROJ_DIR="${REL_TO_HOME%%/*}"
 
-python3 "$@"
+echo "${HOME}/${PROJ_DIR}/scripts/deploysection.py"
+python3 "${HOME}/${PROJ_DIR}/scripts/deploysection.py" "$@"
